5e36bbdc046e9c09e64aa4323362fc6e3a745b49
zsuphakorn <k.suphakorn1995@gmail.com>
2020-05-28 15:30:49 +0700

    Add Server Test

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const http = require("http");
+const server = express();
+const PORT = process.env.PORT||3345;
+
+http.createServer(server).listen(3345, function(){
+    console.log(`Server is running on port : ${PORT}`);
+});
+
+server.get('/', (req, res)=>{
+    res.send(`Successful on port >> ${PORT}`);
+});
+
