d655a6435755a6671954ff4fb7a277866d479573
zsuphakorn <k.suphakorn1995@gmail.com>
2020-05-30 16:29:16 +0700

    change path of server.js #2

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const PORT = process.env.PORT||3345;
 //     console.log(`Server is running on port : ${PORT}`);
 // });
 
-server.get('/', (req, res)=>{
-    if(res.status(200)){
-        res.send('/frontend/home.html');
-    }
+server.get('/', (req, res) =>{
+    res.send('/frontend/home.html');
 });
 
 server.listen(3345, (req, res)=>{
