3c1077d2a8dba81328d2d6358cc6fecbcd9bdb6e
zsuphakorn <k.suphakorn1995@gmail.com>
2020-05-30 15:19:31 +0700

    add homepage

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const http = require("http");
+// const http = require("http");
+// const https = require("https");
 const server = express();
 const PORT = process.env.PORT||3345;
 
-http.createServer(server).listen(3345, function(){
-    console.log(`Server is running on port : ${PORT}`);
-});
+// http.createServer(server).listen(3345, function(){
+//     console.log(`Server is running on port : ${PORT}`);
+// });
 
 server.get('/', (req, res)=>{
-    res.send(`Successful on port >> ${PORT}`);
+    if(res.status(200)){
+        res.send('../frontend/home.js');
+    }
 });
+
+server.listen(3345, (req, res)=>{
+    console.log(`Server is running on port : ${PORT}`);
+})
 
 module.exports(server);
